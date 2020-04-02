a8a650ee3e308c30c2cd336d75bcee1efedd1cdd
Amaury Anciaux <amaury@data-at-work.ch>
2020-04-02 19:19:07 +0200

    Remove Toast notifier

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,19 +1,16 @@
+import logging
 from src.reader.reader import WorkflowGraph
 from src.rules.rules import verify_rules
 from src.configuration import read_configuration
-import logging
-from win10toast import ToastNotifier
 
-toaster = ToastNotifier()
+
 logger = logging.getLogger(__name__)
 
 def analyze_workflow(workflow_path):
-    source = read_configuration().get('analyzer').get('source')
+    source = read_configuration().get('source')
     logger.info(f'Analyzing workflow: {workflow_path}')
     graph = WorkflowGraph(workflow_path, '', source)
     graph.build()
     errors = verify_rules(graph.nodes, graph.containers, source)
-    if errors != True:
-        toaster.show_toast(errors[0]['rule'], errors[0]['message'], threaded=True)
     logger.info(f'Errors generated: {errors}')
     return errors
